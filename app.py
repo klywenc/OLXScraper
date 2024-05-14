5a18fcabe42f76d9a4138fa938e047ccd3568ee4
Megu <43895321+klywenc@users.noreply.github.com>
2024-05-14 21:11:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import redis
 import json
 import pika
 
 app = Flask(__name__)
-redis_db = redis.StrictRedis(host='redis', port=6379, db=0, decode_responses=True)
-
+redis_db = redis.StrictRedis(host='host.docker.internal', port=6379, db=0, decode_responses=True)
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='host.docker.internal'))
 channel = connection.channel()
 channel.queue_declare(queue='scrape_task_queue', durable=True)
+
+
+def get_newest_prices():
+    all_prices_keys = redis_db.keys("all_prices:*")
+    all_prices_keys.sort(reverse=True)
+    newest_prices_key = all_prices_keys[0] if all_prices_keys else None
+    
+    if newest_prices_key:
+        newest_prices = redis_db.smembers(newest_prices_key)
+        return newest_prices
+    else:
+        return set() 
 
 
 @app.route('/')
@@ -25,17 +36,19 @@ def scrape():
     channel.basic_publish(exchange='',
                           routing_key='scrape_task_queue',
                           body=json.dumps(data),
-                          properties=pika.BasicProperties(delivery_mode=2))  
-    return "Scraping started successfully."
+                          properties=pika.BasicProperties(delivery_mode=2))
+    
+    return redirect('/results')
 
-
+@app.route('/results')
 @app.route('/results')
 def results():
     data = {}
     scraped_keys = redis_db.hkeys('scraped')
     for key in scraped_keys:
         data[key] = json.loads(redis_db.hget('scraped', key))
-    return render_template('results.html', data=data)
+    newest_prices = list(get_newest_prices())  # Konwersja zbioru na listę
+    return render_template('results.html', data=data, newest_prices=newest_prices)
 
 
 if __name__ == "__main__":
